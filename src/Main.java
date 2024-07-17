b93cef70cd40876cbb9720e59513cf079871a1b2
Your Name <saqlainzakaria05@gmail.com>
2024-07-17 12:35:47 +0530

    UC1 Added

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+            int IS_FULL_TIME = 1;
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+            if (empCheck == IS_FULL_TIME){
+                System.out.println("Employee is Present");
+            }
+            else{
+                System.out.println("Employee is Absent");
+            }
     }
 }
