257f4df9d2534a2be4f45fddb6b66b98b05e862d
Your Name <saqlainzakaria05@gmail.com>
2024-07-18 13:03:52 +0530

    Added : UseCase03 Part time/Full time updated

--- src/Main.java
+++ src/Main.java
@@ -30,10 +30,33 @@ public class Main {
         empWage = empHours * EMP_RATE_PER_HOUR;
         System.out.println("EmpWage: " + empWage);
     }
+    public static void UC3() {
+
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+
+        int empHours = 0;
+        int empWage  = 0;
+
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_PART_TIME){
+            empHours = 4;
+
+        } else if (empCheck == IS_FULL_TIME) {
+            empHours = 8;
+        } else {
+            empHours = 0;
+        }
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.println("EmpWage: " + empWage);
+    }
 
     public static void main(String[] args) {
         UC1();
         UC2();
+        UC3();
     }
 
 }
